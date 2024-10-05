c3f6ae2dbdd83ff6f6e50862f4a1ac30039ae17a
saul <luckly083@gmail.com>
2024-10-05 11:28:11 -0500

    Added basic middleware to detect token issues

--- front/src/middleware.ts
+++ front/src/middleware.ts
@@ -1,6 +1,22 @@
-export { default } from "next-auth/middleware";
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export const config = {
-  // Protecting only /dashboard path
-  matcher: ["/dashboard/:path*", "/hiring/:path*"],
-};
+// API Paths to be restricted.
+const protectedRoutes = ["/hiring", "/dashboard"];
+
+export default async function middleware(request: NextRequest) {
+  const res = NextResponse.next();
+  const pathname = request.nextUrl.pathname;
+  if (protectedRoutes.some((path) => pathname.startsWith(path))) {
+    const token = await getToken({
+      req: request,
+    });
+    // check not logged in.
+    if (!token) {
+      const url = new URL("/signin", request.url);
+      return NextResponse.redirect(url);
+    }
+  }
+  return res;
+}
