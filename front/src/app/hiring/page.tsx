3e576ca8dbb47649e4aafd0317fc65e1f4eae8d9
saul <luckly083@gmail.com>
2025-08-24 14:21:27 -0500

    log session

--- front/src/app/hiring/page.tsx
+++ front/src/app/hiring/page.tsx
@@ -8,7 +8,7 @@ export const fetchCache = "default-no-store";
 
 export default async function Page() {
   const session: any = await getServerSession(authOptions);
-
+  console.log(session);
   return (
     <main className="px-2 md:px-10 py-6">
       <h1 className="text-2xl font-semibold">
